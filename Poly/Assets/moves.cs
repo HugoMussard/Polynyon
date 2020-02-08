d05aff8f017dff79a4351f9aafa4a00b7f8b383c
Mayes TIDJEDAM <mayes.tidjedam@epita.fr>
2020-02-08 13:08:43 +0100

    premiere salle à co

--- Poly/Assets/moves.cs
+++ Poly/Assets/moves.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class moves : MonoBehaviour
 {
     public CharacterController control;
@@ -25,22 +26,21 @@ public class moves : MonoBehaviour
     void Update()
     {
         isgrounded = Physics.CheckSphere(groundCheck.position, groundis, groundmask);
-        if (isgrounded && velocity.y < 0)
-            velocity.y = -2f;
+            if (isgrounded && velocity.y < 0)
+                velocity.y = -2f;
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         
-        Vector3 move = transform.right * x+ transform.forward * z;
-
-        control.Move(move*speed* Time.deltaTime);
+        Vector3 move = transform.right * x + transform.forward * z;
+        
+        control.Move(move * speed * Time.deltaTime);
 
         velocity.y += gravity * Time.deltaTime;
+
         control.Move(velocity * Time.deltaTime);
+
         if (Input.GetButtonDown("Jump") && isgrounded)
             velocity.y = Mathf.Sqrt(jumphigh * (-2f) * gravity);
-			
-		
-
     }
 }
