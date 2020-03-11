af209dc4c27c61774ee685a298c2e2b290486f4c
Mayes TIDJEDAM <mayes.tidjedam@epita.fr>
2020-03-12 00:00:19 +0100

    portée d'intéraction prise en compte

--- Poly/Assets/Scripts/Interaction.cs
+++ Poly/Assets/Scripts/Interaction.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class Interaction : MonoBehaviour
 {
     public Camera cam;
-         
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,14 +21,33 @@ public class Interaction : MonoBehaviour
 
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
+            Vector3 position = transform.position;
 
             
             if (Physics.Raycast(ray, out hit, 100))
             {
                 Interactable interactable = hit.collider.GetComponent<Interactable>();
+                Vector3 interact = interactable.transform.position;
+                #region check if pos is valid
+                Vector3 diff = position - interact;
+                if (diff.x<0)
+                {
+                    diff.x *= -1;
+                }
+                if (diff.z < 0)
+                {
+                    diff.z *= -1;
+                }
+                if (diff.y < 0)
+                {
+                    diff.y *= -1;
+                }
+                #endregion
                 if (interactable != null)
                 {
-                    interactable.state = true;
+                    float rad = interactable.radius;
+                    if (diff.x<rad && diff.y<rad && diff.z <rad)
+                        interactable.state = true;
                 }
             }
         }
