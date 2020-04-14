986eeaf7015d402e5746b752d7a2783b26bc0b17
Mayes TIDJEDAM <mayes.tidjedam@epita.fr>
2020-04-14 11:02:38 +0200

    énigme terminée

---
+++ Poly/Assets/Scripts/glypheenigma.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class glypheenigma : MonoBehaviour
+{
+    public Interactable glyphe1;
+    public Interactable glyphe2;
+    public Interactable glyphe3;
+    public Interactable glyphe4;
+    public Interactable glyphe5;
+    public Interactable glyphe6;
+    public Interactable opener;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (glyphe1.state || glyphe2.state || glyphe4.state || glyphe6.state)
+        {
+            opener.state = false;
+        }
+        else
+        {
+            if (glyphe3.state && glyphe5.state)
+            {
+                opener.state = true;
+            }
+            else
+            {
+                opener.state = false;
+            }
+        }
+    }
+}
---
+++ Poly/Assets/enigma1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enigma1 : MonoBehaviour
+{
+    Interactable glyphe1;
+    Interactable glyphe2;
+    Interactable glyphe3;
+    Interactable glyphe4;
+    Interactable glyphe5;
+    Interactable glyphe6;
+    Interactable opener;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (glyphe1.state || glyphe2.state || glyphe4.state || glyphe6.state)
+        { }
+        else
+        {
+            if (glyphe3.state && glyphe5.state)
+            {
+                opener.state = true;
+            }
+        }
+    }
+}
