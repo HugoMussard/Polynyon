9c05d4d5a4d87282ecbb8682c91a217d87c9a68d
Mayes TIDJEDAM <mayes.tidjedam@epita.fr>
2020-04-17 01:29:26 +0200

    L'énigme 1 est finalisée et random

--- Poly/Assets/Scripts/Premiere_salle/glypheenigma.cs
+++ Poly/Assets/Scripts/Premiere_salle/glypheenigma.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class glypheenigma : MonoBehaviour
 {
@@ -11,23 +13,70 @@ public class glypheenigma : MonoBehaviour
     public Interactable glyphe5;
     public Interactable glyphe6;
     public Interactable opener;
+    public int enigm = 0;
 
+
+    void Start()
+    {
+        System.Random random = new System.Random();
+        enigm = random.Next(3);
+
+    }
     // Update is called once per frame
     void Update()
     {
-        if (glyphe1.state || glyphe2.state || glyphe4.state || glyphe6.state)
+        if (enigm == 0)
         {
-            opener.state = false;
-        }
-        else
-        {
-            if (glyphe3.state && glyphe5.state)
+            if (glyphe1.state || glyphe2.state || glyphe4.state || glyphe6.state)
             {
-                opener.state = true;
+                opener.state = false;
             }
             else
             {
+                if (glyphe3.state && glyphe5.state)
+                {
+                    opener.state = true;
+                }
+                else
+                {
+                    opener.state = false;
+                }
+            }
+        }
+        if (enigm == 1)
+        {
+            if (glyphe1.state || glyphe3.state || glyphe4.state || glyphe5.state)
+            {
                 opener.state = false;
+            }
+            else
+            {
+                if (glyphe6.state && glyphe2.state)
+                {
+                    opener.state = true;
+                }
+                else
+                {
+                    opener.state = false;
+                }
+            }
+        }
+        if (enigm == 2)
+        {
+            if (glyphe1.state || glyphe3.state || glyphe4.state || glyphe6.state)
+            {
+                opener.state = false;
+            }
+            else
+            {
+                if (glyphe2.state && glyphe5.state)
+                {
+                    opener.state = true;
+                }
+                else
+                {
+                    opener.state = false;
+                }
             }
         }
     }
---
+++ Poly/Assets/Scripts/Premiere_salle/indice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class indice : MonoBehaviour
+{
+    public glypheenigma enigma;
+    public Material mat0;
+    public Material mat1;
+    public Material mat2;
+    public Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enigma.enigm == 0)
+            rend.material = mat0;
+        if (enigma.enigm == 1)
+            rend.material = mat1;
+        if (enigma.enigm == 2)
+            rend.material = mat2;
+    }
+}
