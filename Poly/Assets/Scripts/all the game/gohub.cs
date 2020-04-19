59fcc36aab6cf0b2154c13d108e2519695614c38
Mayes TIDJEDAM <mayes.tidjedam@epita.fr>
2020-04-19 22:45:22 +0200

    gohub done, hub en cours

---
+++ Poly/Assets/Scripts/all the game/gohub.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gohub : MonoBehaviour
+{
+    public LayerMask hub;
+    public float groundis = 0.5f;
+    public Transform Hubcheck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.CheckSphere(Hubcheck.position, groundis, hub))
+        {
+            SceneManager.LoadSceneAsync(13);
+            SceneManager.UnloadSceneAsync(12);
+        }
+    }
+}
