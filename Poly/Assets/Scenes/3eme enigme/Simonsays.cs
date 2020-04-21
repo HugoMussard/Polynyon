3bebc46626c30cd190fc644581d684c33af73e6f
Martin LACAILLE <martin.lacaille@epita.fr>
2020-04-21 19:10:18 +0200

    voila la 3eme enigme

---
+++ Poly/Assets/Scenes/3eme enigme/Simonsays.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Simonsays : MonoBehaviour
+{
+    /*
+     * correspondance de couleurs :
+     * 0 -> rouge
+     * 1 -> vert
+     * 2 -> bleu
+     * 3 -> jaune
+     *
+     * 
+     */
+
+    private int timePause = 1; //la rapidite de l'espacement des lumieres
+
+    private int Time = 30; //temps en seconde a laquelle se fait chaque enigmes
+
+    public Text disp;
+    // Start is called before the first frame update
+
+    void lighton(int light)
+    {
+        switch (light)
+        {
+            case 1:
+                //allumer lumiere rouge, etc...
+                break;
+            
+        }
+        
+        
+    }
+    
+    void lightoff(int light)
+    {
+        switch (light)
+        {
+            case 1:
+            //eteindre lumiere rouge, etc...
+            break;
+            
+        }
+        
+        
+    }
+
+    void waitFor(int seconds)
+    {
+        timer = 0;
+        while (timer < seconds)
+            timer += Time.deltaTime;
+
+        return;
+    }
+
+    void displaylevel(int[] lights)
+    {
+        foreach (var light in lights)
+        {
+            lighton(light);
+            waitFor(timePause);
+            lightoff(light);
+            waitFor(timePause);
+
+        }
+        
+        
+    }
+
+    int adatp(int light, int modifier)
+    {
+        return (light + modifier) % 4;
+
+    }
+
+    int[] getactions(int length)
+    {
+        int[] actions = new int[length];
+        
+        for(int i=0; i< length; i++)
+        //retournes la touche pressee
+        
+    }
+    
+    bool ClickLevel(int[] lights, int modifier)
+    {
+        for (int i = 0; i < lights.Length; i++)
+        {
+            lights[i] = adatp(i, modifier);
+        }
+
+        int[] actions = getactions(lights.Length);
+
+        for (int i = 0; i < lights.Length; i++)
+        {
+            if (actions[i] != lights[i])
+                return false;
+        }
+
+        return true;
+    }
+
+    private int TimeRemaining;
+
+    void displaytext(int i)
+    {
+        // afficher numero de la manche (i+1) et timeremaining
+    }
+    
+    void Start()
+    {
+        TimeRemaining = Time;
+
+
+        for (int i = 0; i < 3; i++)
+        {
+            displaytext(i);
+            int[] lights = createRandomLights(2*i + 4);
+            displaylevel(lights);
+            if (!ClickLevel(lights, i))
+                i = 0;
+
+        }
+        
+        //l'enigme est finie
+    }
+
+    int[] createRandomLights(int length)
+    {
+        int[] lights = new int[length];
+
+        var rand = new Random();
+
+        for (int i = 0; i < length; i++)
+        {
+            lights[i] = rand.Next(4);
+        }
+
+        return lights;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
