3fa8f2eef6b8465ffe0b61bcf0e0fc2ad00c1308
Mayes TIDJEDAM <mayes.tidjedam@epita.fr>
2020-04-22 20:33:17 +0200

    déco énigme 2 done

---
+++ Poly/Assets/Scripts/all the game/pression_plate_hub.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pression_plate_hub : MonoBehaviour
+{
+    public Renderer rend;
+    public Material on;
+    public Material off;
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+
+    }
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.isTrigger)
+            rend.material = on;
+        else
+            rend.material = off;
+    }
+}
