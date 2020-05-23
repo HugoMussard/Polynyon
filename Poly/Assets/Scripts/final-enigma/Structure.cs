d60158da92739aaf2e77c559e68563a5f071f70c
Lissieur <60755629+Lissieur@users.noreply.github.com>
2020-05-23 23:45:25 +0200

    just saving ( final enigma )

---
+++ Poly/Assets/Scripts/final-enigma/Structure.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Structure : MonoBehaviour
+{
+    public bool appear;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        appear = false;
+        anim = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        anim.enabled = appear;
+    }
+}
