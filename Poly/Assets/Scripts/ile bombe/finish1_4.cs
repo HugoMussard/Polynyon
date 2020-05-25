8ac5e5d937c4c9678bd36fce161813e43aae3c9c
Lissieur <60755629+Lissieur@users.noreply.github.com>
2020-05-25 17:50:49 +0200

    tous les links et les saves sont done

---
+++ Poly/Assets/Scripts/ile bombe/finish1_4.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class finish1_4 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnTriggerEnter(Collider other)
+    {
+        PlayerPrefs.SetInt("1-4", 1);
+    }
+}
